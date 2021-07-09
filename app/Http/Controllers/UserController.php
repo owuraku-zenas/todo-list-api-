e39277f5aeb91332cbeb8193e8f50de06db249ba
Zenas Kwaku Awuku <zenasawuku@gmail.com>
2021-07-09 14:56:58 +0000

    Creating category and todo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,11 +16,6 @@ class UserController extends Controller
 
     public function logout(Request $request)
     {
-        // Auth::logout();
-
-        // $request->session()->invalidate();
-
-        // $request->user()->token()->revoke();
 
         $request->user()->currentAccessToken()->delete();
 
